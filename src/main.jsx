e3071d3d01ad8b24bceb3a1e34a4f0867bca9e6f
kripesh03 <kripeshpoudel9842@gmail.com>
2025-02-16 18:35:07 +0545

    cart page completed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-
+import "sweetalert2/dist/sweetalert2.js"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/products/CartPage";
 
 
 const router= createBrowserRouter([
@@ -31,6 +32,10 @@ const router= createBrowserRouter([
             {
                 path : '/register',
                 element: <Register/>
+            },
+            {
+                path : '/cart',
+                element: <CartPage/>
             }
         ]
     },
