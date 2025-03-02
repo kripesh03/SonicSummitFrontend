5a3362592dff5b2145c9c84a8a59e865ef0cedb3
kripesh03 <kripeshpoudel9842@gmail.com>
2025-03-02 12:56:58 +0545

    admin login test

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -69,7 +69,7 @@ const AdminLogin = () => {
                     message && <p className='text-red-500 text-xs italic mb-3'>{message}</p>
                 }
                 <div className='w-full'>
-                    <button className='bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Login </button>
+                    <button className='bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none' type='submit' >Login </button>
                 </div>
             </form>
 
